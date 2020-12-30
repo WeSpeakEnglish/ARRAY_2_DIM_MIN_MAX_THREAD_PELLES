cd4eb8fbd5c00012f93a92b1717df02f04dec840
Your Name <you@example.com>
2020-12-30 15:37:49 +0300

    tested algorithm

--- ARRAY_2_DIM_MIN_MAX/main.c
+++ ARRAY_2_DIM_MIN_MAX/main.c
@@ -3,11 +3,12 @@
 #include <Windows.h>
 #include <stdint.h>
 #include <math.h>
+#include "nortos.h"
 
 #define CHANNELS_N      2   //channels of ADC collecting
 #define BUFFER_SIZE     16// points for each ADC channel (collected in total)
-#define STEP_AMP_MEAN   4 // how many points to average (steep control) (attack ) for each the channels
-#define THRESHOLD_VALUE 700  //hihger apmlithude = increade Attenuation, lower = decrease ATT
+#define STEP_AMP_MEAN   8 // how many points to average (steep control) (attack ) for each the channels
+#define THRESHOLD_VALUE 500  //hihger apmlithude = increade Attenuation, lower = decrease ATT
 #define MIDDLE_VALUE    2048  //cold be autocalibrated with muted input signal
 
 volatile uint16_t bufferADC[BUFFER_SIZE][CHANNELS_N];    //buffer of ADC
@@ -15,6 +16,39 @@ volatile uint16_t position;                              //position inside the b
 
  int counterThread = 0;
  int counterMain = 0 ;
+ int counterArray = 0;
+
+ int amp1 = MIDDLE_VALUE;
+ int amp2 = MIDDLE_VALUE;
+
+int module (int A){
+ return (A < 0)? -A: A;
+}
+ int ApmControl(int part){
+     int amp1ch = 0;
+     int amp2ch = 0;
+     int amp1max = 0;
+     int amp2max = 0;
+
+     for (int i =0; i< STEP_AMP_MEAN; i++){
+        if(part){
+            amp1ch = module(bufferADC[STEP_AMP_MEAN+i][0] - MIDDLE_VALUE);
+            amp2ch = module(bufferADC[STEP_AMP_MEAN+i][1] - MIDDLE_VALUE);
+        }
+        else{
+            amp1ch = module(bufferADC[i][0] - MIDDLE_VALUE);
+            amp2ch = module(bufferADC[i][1] - MIDDLE_VALUE);
+        }
+    if (amp1ch > amp1max)amp1max = amp1ch;
+    if (amp2ch > amp2max)amp2max = amp2ch;
+     }
+ if (amp1max > THRESHOLD_VALUE) amp1--;
+ else amp1++;
+ if (amp2max > THRESHOLD_VALUE) amp2--;
+ else amp2++;
+
+ printf ("\n amp1max: %d   amp2max: %d\n", amp1max, amp2max);
+ }
 
 DWORD WINAPI DoStuff(LPVOID lpParameter)
 {
@@ -28,13 +62,24 @@ DWORD WINAPI DoStuff(LPVOID lpParameter)
 
     Sleep(2000);
      //fill with sin
-     *pointerCH1 = 2048 + (uint16_t)2048.0*sin((float)counterThread/2.0);
+     *pointerCH1 = MIDDLE_VALUE + (uint16_t)(float)amp1*sin((float)counterThread/2.0);
      pointerCH1+=2;
+     counterArray++;
  //    pointerCH1++;
      if (pointerCH1 > &bufferADC[BUFFER_SIZE -1][0]) pointerCH1 = &bufferADC[0][0];
 
-     *pointerCH2 = 2048 + (uint16_t)2048.0*sin((float)counterThread/2.7 + 0.17);
+     *pointerCH2 = MIDDLE_VALUE + (uint16_t)(float)amp1*sin((float)counterThread/2.7 + 0.17);
      pointerCH2+=2;
+     counterArray++;
+
+     counterArray %= BUFFER_SIZE*CHANNELS_N;
+
+     if(counterArray == BUFFER_SIZE){
+            ApmControl(0);
+     }
+     if(counterArray == 0){
+            ApmControl(1);
+     }
  //    pointerCH1++;
      if (pointerCH2 > &bufferADC[BUFFER_SIZE -1][1]) pointerCH2 = &bufferADC[0][1];
     //printf("%d\n",counterThread++);
@@ -60,6 +105,7 @@ DWORD WINAPI DoStuff(LPVOID lpParameter)
 
 int main()
 {
+    FP_QueueIni();
     // Create a new thread which will start at the DoStuff function
     HANDLE hThread = CreateThread(
         NULL,    // Thread attributes
