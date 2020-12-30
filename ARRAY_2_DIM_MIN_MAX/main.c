5099477e96c0f8c1a9c8d7f95a1160805b1c6b5a
Your Name <you@example.com>
2020-12-30 16:18:54 +0300

    updated with auto test

--- ARRAY_2_DIM_MIN_MAX/main.c
+++ ARRAY_2_DIM_MIN_MAX/main.c
@@ -18,20 +18,20 @@ volatile uint16_t position;                              //position inside the b
  int counterMain = 0 ;
  int counterArray = 0;
 
- int amp1 = MIDDLE_VALUE;
- int amp2 = MIDDLE_VALUE;
+ int amp1sin = 200;
+ int amp2sin = 2000;
 
 int module (int A){
  return (A < 0)? -A: A;
 }
- int ApmControl(int part){
+ int ApmControl(struct fParams * Cont){
      int amp1ch = 0;
      int amp2ch = 0;
      int amp1max = 0;
      int amp2max = 0;
 
      for (int i =0; i< STEP_AMP_MEAN; i++){
-        if(part){
+        if(Cont->IntVar){
             amp1ch = module(bufferADC[STEP_AMP_MEAN+i][0] - MIDDLE_VALUE);
             amp2ch = module(bufferADC[STEP_AMP_MEAN+i][1] - MIDDLE_VALUE);
         }
@@ -42,12 +42,12 @@ int module (int A){
     if (amp1ch > amp1max)amp1max = amp1ch;
     if (amp2ch > amp2max)amp2max = amp2ch;
      }
- if (amp1max > THRESHOLD_VALUE) amp1--;
- else amp1++;
- if (amp2max > THRESHOLD_VALUE) amp2--;
- else amp2++;
+ if (amp1max > THRESHOLD_VALUE) amp1sin--;
+ else amp1sin++;
+ if (amp2max > THRESHOLD_VALUE) amp2sin--;
+ else amp2sin++;
 
- printf ("\n amp1max: %d   amp2max: %d\n", amp1max, amp2max);
+ printf ("\n amp1max: %d, amp1sin: %d;   amp2max: %d, amp2sin: %d\n", amp1max, amp1sin, amp2max, amp2sin);
  }
 
 DWORD WINAPI DoStuff(LPVOID lpParameter)
@@ -60,34 +60,34 @@ DWORD WINAPI DoStuff(LPVOID lpParameter)
     // The new thri<ead will start here
     for(;;){
 
-    Sleep(2000);
+    Sleep(1000);
      //fill with sin
-     *pointerCH1 = MIDDLE_VALUE + (uint16_t)(float)amp1*sin((float)counterThread/2.0);
+     *pointerCH1 = MIDDLE_VALUE + (uint16_t)(float)amp1sin*sin((float)counterThread/2.0);
      pointerCH1+=2;
      counterArray++;
  //    pointerCH1++;
      if (pointerCH1 > &bufferADC[BUFFER_SIZE -1][0]) pointerCH1 = &bufferADC[0][0];
 
-     *pointerCH2 = MIDDLE_VALUE + (uint16_t)(float)amp1*sin((float)counterThread/2.7 + 0.17);
+     *pointerCH2 = MIDDLE_VALUE + (uint16_t)(float)amp2sin*sin((float)counterThread/2.7 + 0.17);
      pointerCH2+=2;
      counterArray++;
 
      counterArray %= BUFFER_SIZE*CHANNELS_N;
 
      if(counterArray == BUFFER_SIZE){
-            ApmControl(0);
+            FP_push(ApmControl,&(struct fParams){0,0.0});
      }
      if(counterArray == 0){
-            ApmControl(1);
+            FP_push(ApmControl,&(struct fParams){1,0.0});
      }
  //    pointerCH1++;
      if (pointerCH2 > &bufferADC[BUFFER_SIZE -1][1]) pointerCH2 = &bufferADC[0][1];
     //printf("%d\n",counterThread++);
 
-    printf("Addr: bufferADC[BUFFER_SIZE -1][0]: %d\n",(int)&bufferADC[BUFFER_SIZE -1][0]);
-    printf("Addr: pointerCH1: %d\n\n",(int)pointerCH1);
-    printf("Addr: bufferADC[BUFFER_SIZE -1][1]: %d\n",(int)&bufferADC[BUFFER_SIZE -1][1]);
-    printf("Addr: pointerCH2: %d\n\n",(int)pointerCH2);
+  //  printf("Addr: bufferADC[BUFFER_SIZE -1][0]: %d\n",(int)&bufferADC[BUFFER_SIZE -1][0]);
+  //  printf("Addr: pointerCH1: %d\n\n",(int)pointerCH1);
+  //  printf("Addr: bufferADC[BUFFER_SIZE -1][1]: %d\n",(int)&bufferADC[BUFFER_SIZE -1][1]);
+  //  printf("Addr: pointerCH2: %d\n\n",(int)pointerCH2);
 
     for(int i = 0; i < CHANNELS_N; i++){
      printf("\nCH%d\n",i + 1);
@@ -98,7 +98,7 @@ DWORD WINAPI DoStuff(LPVOID lpParameter)
     }
     printf("\n----------------------------------------\n");
     counterThread++;
-    c = getc(stdin);
+  //  c = getc(stdin);
     }
     return 0;
 }
@@ -125,9 +125,10 @@ int main()
   //
     printf("BUFFER bufferADC contain:\n");
     for(;;){
-    Sleep(2000);
+    Sleep(1000);
   //  printf("Main thread: #%d\n",counterMain++);
 
+    FP_pull();
     }
     WaitForSingleObject(hThread, INFINITE);
     // Thread handle must be closed when no longer needed
